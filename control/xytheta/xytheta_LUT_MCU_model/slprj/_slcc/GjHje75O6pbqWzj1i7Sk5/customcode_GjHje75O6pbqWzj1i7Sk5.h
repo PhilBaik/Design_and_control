35ae7f610ef43ad092a89747b604cbacbc24dbb2
백형민(공과대학 전기전자공학) <qorgud1209@yonsei.ac.kr>
2022-05-25 22:35:29 +0900

    feat: renewing files, LUT simulink
    folder hierarchy 최신화, 각 plot 추가, LUT simulink 자동화

---
+++ control/xytheta/xytheta_LUT_MCU_model/slprj/_slcc/GjHje75O6pbqWzj1i7Sk5/customcode_GjHje75O6pbqWzj1i7Sk5.h
@@ -0,0 +1,38 @@
+#ifndef __customcode_GjHje75O6pbqWzj1i7Sk5_h__
+#define __customcode_GjHje75O6pbqWzj1i7Sk5_h__
+
+/* Include files */
+#include "mex.h"
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "tmwtypes.h"
+
+
+/* Helper definitions for DLL support */
+#if defined _WIN32 
+  #define DLL_EXPORT_CC __declspec(dllexport)
+#else
+  #if __GNUC__ >= 4
+    #define DLL_EXPORT_CC __attribute__ ((visibility ("default")))
+  #else
+    #define DLL_EXPORT_CC
+  #endif
+#endif
+/* Custom Code from Simulation Target dialog */
+#include "microcontroller.h"
+
+
+/* Function Declarations */
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define customcode_GjHje75O6pbqWzj1i7Sk5_initializer()
+
+#define customcode_GjHje75O6pbqWzj1i7Sk5_terminator()
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
