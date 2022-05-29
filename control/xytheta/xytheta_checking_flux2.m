041db0a7a8d067eaf6fe81e5d53cab4335c34e6f
백형민(공과대학 전기전자공학) <qorgud1209@yonsei.ac.kr>
2022-05-29 23:55:18 +0900

    feat: automatically updating kg value

--- control/xytheta/xytheta_checking_flux2.m
+++ control/xytheta/xytheta_checking_flux2.m
@@ -15,69 +15,69 @@ load('femm_output.mat')
 % a1a2a3a4 b1b2b3b4 c1c2c3c4
 % too many figures
 % 
-for m = x_index
-    for n = 1:1:4
-        f(n,m) = figure; %a
-        hold on;
-        legend("off")
-        legend("show",Location="best")
-        title(['x = ', num2str(x_position(1,m)),  '  sensor r=  ',num2str(sensor_r),' a',num2str(n)])
-        f(n+4,m) = figure; %b
-        hold on;
-        legend("off")
-        legend("show",Location="best")
-        title(['x = ', num2str(x_position(1,m)),  '  sensor r=  ',num2str(sensor_r),' b',num2str(n)])
-        f(n+8,m) = figure; %c
-        hold on;
-        legend("off")
-        legend("show",Location="best")
-        title(['x = ', num2str(x_position(1,m)),  '  sensor r=  ',num2str(sensor_r),' c',num2str(n)])
-    end
-end
-
-for m = x_index
-    for n = y_index
-        figure(f(1,m));
-        plot(theta_r_in,a1(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(2,m));
-        plot(theta_r_in,b1(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(3,m));
-        plot(theta_r_in,c1(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(4,m));
-        plot(theta_r_in,a2(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(5,m));
-        plot(theta_r_in,b2(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(6,m));
-        plot(theta_r_in,c2(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(7,m));
-        plot(theta_r_in,a3(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(8,m));
-        plot(theta_r_in,b3(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(9,m));
-        plot(theta_r_in,c3(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(10,m));
-        plot(theta_r_in,a4(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(11,m));
-        plot(theta_r_in,b4(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-        figure(f(12,m));
-        plot(theta_r_in,c4(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
-    end
-end
-
-max_peak_flux = zeros(length(x_index),length(y_index));
-min_peak_flux = zeros(length(x_index),length(y_index));
-
-
-for x = x_index
-    for y = y_index
-        max_arr = [max(abs(a1(:,x,y))),max(abs(a2(:,x,y))),max(abs(a3(:,x,y))),max(abs(a4(:,x,y))), ...
-            max(abs(b1(:,x,y))),max(abs(b2(:,x,y))),max(abs(b3(:,x,y))),max(abs(b4(:,x,y))) ...
-            ,max(abs(c1(:,x,y))),max(abs(c2(:,x,y))),max(abs(c3(:,x,y))),max(abs(c4(:,x,y)))];
-        max_peak_flux(x,y) = max(max_arr);
-        min_peak_flux(x,y) = min(max_arr);
-        diff = a1(:,x,y)-a2(:,x,y);
-    end
-end
+% for m = x_index
+%     for n = 1:1:4
+%         f(n,m) = figure; %a
+%         hold on;
+%         legend("off")
+%         legend("show",Location="best")
+%         title(['x = ', num2str(x_position(1,m)),  '  sensor r=  ',num2str(sensor_r),' a',num2str(n)])
+%         f(n+4,m) = figure; %b
+%         hold on;
+%         legend("off")
+%         legend("show",Location="best")
+%         title(['x = ', num2str(x_position(1,m)),  '  sensor r=  ',num2str(sensor_r),' b',num2str(n)])
+%         f(n+8,m) = figure; %c
+%         hold on;
+%         legend("off")
+%         legend("show",Location="best")
+%         title(['x = ', num2str(x_position(1,m)),  '  sensor r=  ',num2str(sensor_r),' c',num2str(n)])
+%     end
+% end
+% 
+% for m = x_index
+%     for n = y_index
+%         figure(f(1,m));
+%         plot(theta_r_in,a1(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(2,m));
+%         plot(theta_r_in,b1(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(3,m));
+%         plot(theta_r_in,c1(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(4,m));
+%         plot(theta_r_in,a2(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(5,m));
+%         plot(theta_r_in,b2(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(6,m));
+%         plot(theta_r_in,c2(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(7,m));
+%         plot(theta_r_in,a3(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(8,m));
+%         plot(theta_r_in,b3(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(9,m));
+%         plot(theta_r_in,c3(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(10,m));
+%         plot(theta_r_in,a4(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(11,m));
+%         plot(theta_r_in,b4(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%         figure(f(12,m));
+%         plot(theta_r_in,c4(:,m,n),'DisplayName',['y = ',num2str(y_position(1,n))])
+%     end
+% end
+% 
+% max_peak_flux = zeros(length(x_index),length(y_index));
+% min_peak_flux = zeros(length(x_index),length(y_index));
+% 
+% 
+% for x = x_index
+%     for y = y_index
+%         max_arr = [max(abs(a1(:,x,y))),max(abs(a2(:,x,y))),max(abs(a3(:,x,y))),max(abs(a4(:,x,y))), ...
+%             max(abs(b1(:,x,y))),max(abs(b2(:,x,y))),max(abs(b3(:,x,y))),max(abs(b4(:,x,y))) ...
+%             ,max(abs(c1(:,x,y))),max(abs(c2(:,x,y))),max(abs(c3(:,x,y))),max(abs(c4(:,x,y)))];
+%         max_peak_flux(x,y) = max(max_arr);
+%         min_peak_flux(x,y) = min(max_arr);
+%         diff = a1(:,x,y)-a2(:,x,y);
+%     end
+% end
 
 max_peak_flux
 min_peak_flux
