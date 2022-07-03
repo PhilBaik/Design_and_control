9710a66bf8bdfe1720061b3c2f85a68fad69fa03
백형민(공과대학 전기전자공학) <qorgud1209@yonsei.ac.kr>
2022-07-03 18:29:44 +0900

    feat: 0629 plot

--- control/xytheta/xytheta_plot.m
+++ control/xytheta/xytheta_plot.m
@@ -3,14 +3,13 @@ clc;
 clear;
 close all;
 
-load("xytheta_output_sensor_r48_kg3999_20220624_1115.mat")
+load("xytheta_output_sensor_r48_kg3999_20220629_1432.mat")
 % load("hi2.mat")
 %% 
 x1 = x_mid+2;
 y0 = y_mid;
 date = datestr(now,'yyyymmdd_HHMM');
-theta_check = theta_elec_in;
-
+save_enable = 0;
 %% getting abc1 abc2 and its corresponding dq
 a1_1 = a1(:,x_mid+2,y_mid)';
 a2_1 = a2(:,x_mid+2,y_mid)';
@@ -37,12 +36,12 @@ ax = gca;
 xticks([0 90 180 270 360])
 ax.TickLabelInterpreter='latex';
 xlim([0,360])
-plot(theta_check,a1_1,'-r','LineWidth',1)
-plot(theta_check,a2_1,'--r','LineWidth',1)
-plot(theta_check,b1_1,'-b','LineWidth',1)
-plot(theta_check,b2_1,'--b','LineWidth',1)
-plot(theta_check,c1_1,'-g','LineWidth',1)
-plot(theta_check,c2_1,'--g','LineWidth',1)
+plot(theta_elec_in,a1_1,'-r','LineWidth',1)
+plot(theta_elec_in,a2_1,'--r','LineWidth',1)
+plot(theta_elec_in,b1_1,'-b','LineWidth',1)
+plot(theta_elec_in,b2_1,'--b','LineWidth',1)
+plot(theta_elec_in,c1_1,'-g','LineWidth',1)
+plot(theta_elec_in,c2_1,'--g','LineWidth',1)
 hold off;
 box on;
 ylatex = '$B[T]$';
@@ -54,8 +53,11 @@ leg_abc = legend('$B_{a1}$','$B_{a2}$','$B_{b1}$','$B_{b2}$','$B_{c1}$','$B_{c2}
 set(leg_abc, 'Interpreter', 'latex');
 set(leg_abc, 'FontSize', 15);
 
+if(save_enable==1)
 filename = sprintf('abc1abc2_sensor_r%d_%s.png',sensor_r,date);
 % saveas(figure_abc12,filename,'png')
+exportgraphics(ax,filename,'Resolution',600)
+end
 %% alpha beta gamma 1, 2
 figure_alpha = figure;
 hold on;
@@ -65,12 +67,12 @@ ax = gca;
 xticks([0 90 180 270 360])
 ax.TickLabelInterpreter='latex';
 xlim([0,360])
-plot(theta_check,alpha1_1,'-r','LineWidth',1)
-plot(theta_check,alpha2_1,'--r','LineWidth',1)
-plot(theta_check,beta1_1,'-b','LineWidth',1)
-plot(theta_check,beta2_1,'--b','LineWidth',1)
-plot(theta_check,gamma1_1,'-g','LineWidth',1)
-plot(theta_check,gamma2_1,'--g','LineWidth',1)
+plot(theta_elec_in,alpha1_1,'-r','LineWidth',1)
+plot(theta_elec_in,alpha2_1,'--r','LineWidth',1)
+plot(theta_elec_in,beta1_1,'-b','LineWidth',1)
+plot(theta_elec_in,beta2_1,'--b','LineWidth',1)
+plot(theta_elec_in,gamma1_1,'-g','LineWidth',1)
+plot(theta_elec_in,gamma2_1,'--g','LineWidth',1)
 hold off;
 box on;
 ylatex = '$B[T]$';
@@ -82,8 +84,11 @@ leg_abc = legend('$B_{\alpha1}$','$B_{\alpha2}$','$B_{\beta1}$','$B_{\beta2}$','
 set(leg_abc, 'Interpreter', 'latex')
 set(leg_abc, 'FontSize', 15);
 
+if(save_enable==1)
 filename2 = sprintf('alphabetagamma_sensor_r%d_%s.png',sensor_r,date);
 % saveas(figure_alpha,filename2,'png');
+exportgraphics(ax,filename2,'Resolution',600)
+end
 %% x12,y12,x34,y34
 x12_x1y0= 1000*x12(:,x_mid+2,y_mid);
 x34_x1y0= 1000*x34(:,x_mid+2,y_mid);
@@ -101,12 +106,12 @@ ax = gca;
 xticks([0 90 180 270 360])
 ax.TickLabelInterpreter='latex';
 xlim([0,360]);
-plot(theta_check,x12_x1y0,'-r','LineWidth',1)
-plot(theta_check,x34_x1y0,'--r','LineWidth',1)
-plot(theta_check,y12_x1y0,'-b','LineWidth',1)
-plot(theta_check,y34_x1y0,'--b','LineWidth',1)
-plot(theta_check,x12x34_x1y0,'-g','LineWidth',1)
-plot(theta_check,y12y34_x1y0,'--g','LineWidth',1)
+plot(theta_elec_in,x12_x1y0,'-r','LineWidth',1)
+plot(theta_elec_in,x34_x1y0,'--r','LineWidth',1)
+plot(theta_elec_in,y12_x1y0,'-b','LineWidth',1)
+plot(theta_elec_in,y34_x1y0,'--b','LineWidth',1)
+plot(theta_elec_in,x12x34_x1y0,'-g','LineWidth',1)
+plot(theta_elec_in,y12y34_x1y0,'--g','LineWidth',1)
 hold off;
 box on;
 ylatex = '$B[mT]$';
@@ -118,8 +123,11 @@ leg_abc = legend('$x_{12}$','$x_{34}$','$y_{12}$','$y_{34}$','$x_{12}+x_{34}$','
 set(leg_abc, 'Interpreter', 'latex')
 set(leg_abc, 'FontSize', 15);
 
+if(save_enable==1)
 filename = sprintf('x12x34y12y34_sensor_r%d_%s.png',sensor_r,date);
 % saveas(figure_x12x34,filename,'png');
+exportgraphics(ax,filename,'Resolution',600)
+end
 %% full figure
 full_figure = figure;
 hold on;
@@ -154,8 +162,11 @@ xlatex = '$\hat{x}\,[mm]$';
 xlabel(xlatex,'Interpreter','latex')
 hold off
 
+if(save_enable==1)
 filename = sprintf('xy_total_sensor_r%d_%s.png',sensor_r,date);
 % saveas(full_figure,filename);
+exportgraphics(ax,filename,'Resolution',600)
+end
 %% x=1 y=0
 
 
@@ -168,8 +179,8 @@ hold on;
 ax = gca;
 xticks([0 90 180 270 360])
 ax.TickLabelInterpreter='latex';
-plot(theta_check,xhat1,'k','Linewidth',2)
-plot(theta_check,yhat0,'b','Linewidth',2)
+plot(theta_elec_in,xhat1,'k','Linewidth',2)
+plot(theta_elec_in,yhat0,'b','Linewidth',2)
 leg_10 = legend('$\hat{x}$','$\hat{y}$','Location','best');
 set(leg_10, 'Interpreter', 'latex');
 set(leg_10, 'FontSize', 15);
@@ -184,10 +195,11 @@ xlatex = '$\theta_r\,[degree]$';
 xlabel(xlatex,'Interpreter','latex')
 hold off
 
+if(save_enable==1)
 filename = sprintf('x1y0_partial_sensor_r%d_%s.png',sensor_r,date);
 % saveas(figx1y0,filename,'png')
-
-
+exportgraphics(ax,filename,'Resolution',600)
+end
 %% theta estimation
 
 theta1_hatx1y0 = theta1hat(:,x1,y0)*180/pi();
@@ -196,14 +208,12 @@ theta2_hatx1y0 = theta2hat(:,x1,y0)*180/pi();
 theta2_hatx1y0 = theta2_hatx1y0.';
 theta_hatx1y0 = theta_r_hat(:,x1,y0)*180/pi();
 theta_hatx1y0 = theta_hatx1y0.';
-theta_in_elecx1y0 = zeros(1,length(theta_r_index));
+theta_elec_inx1y0 = zeros(1,length(theta_r_index));
 
 for i = theta_r_index
-    if theta_elec_in(1,i)>180
-        theta_in_elecx1y0(1,i)=theta_elec_in(1,i)-360;
-    else
-        theta_in_elecx1y0(1,i)=theta_elec_in(1,i);
-    end
+
+        theta_elec_inx1y0(1,i)=theta_elec_in(1,i);
+
 end
 
 figure_theta = figure;
@@ -214,9 +224,10 @@ ax = gca;
 xticks([0 90 180 270 360])
 ax.TickLabelInterpreter='latex';
 
-plot(theta_elec_in,theta_hatx1y0,'k','LineWidth',1)
+
 plot(theta_elec_in,theta1_hatx1y0,'r','LineWidth',1)
 plot(theta_elec_in,theta2_hatx1y0,'b','LineWidth',1)
+plot(theta_elec_in,theta_hatx1y0,'k','LineWidth',1)
 ylim([-200 200])
 xlim([0 360])
 hold off;
@@ -229,10 +240,11 @@ leg_abc = legend('$\hat{\theta}_r$','${\theta}_1$','${\theta}_2$','Location','no
 set(leg_abc, 'Interpreter', 'latex')
 set(leg_abc, 'FontSize', 15);
 
+if(save_enable==1)
 filename = sprintf('theta_x1y0_sensor_r%d_%s.png',sensor_r,date);
 % saveas(figure_theta,filename,'png');
-
-
+exportgraphics(ax,filename,'Resolution',600)
+end
 %% theta err estimation
 
 figure_err_theta = figure;
@@ -243,19 +255,31 @@ ax = gca;
 xticks([0 90 180 270 360])
 ax.TickLabelInterpreter='latex';
 
-theta_err1 = theta1_hatx1y0-theta_in_elecx1y0;
-theta_err2 = theta2_hatx1y0-theta_in_elecx1y0;
-theta_err = theta_hatx1y0-theta_in_elecx1y0;
+theta_err1 = mod(theta1_hatx1y0-theta_elec_inx1y0,360);
+theta_err2 = mod(theta2_hatx1y0-theta_elec_inx1y0,360);
+theta_err = mod(theta_hatx1y0-theta_elec_inx1y0,360);
 
+for i = theta_r_index
+    if(theta_err1(1,i)>180)
+        theta_err1(1,i)=theta_err1(1,i)-360;
+    end
+    if(theta_err2(1,i)>180)
+        theta_err2(1,i)=theta_err2(1,i)-360;
+    end
+    if(theta_err(1,i)>180)
+        theta_err(1,i)=theta_err(1,i)-360;
+    end
+    
 
+end
 
 err_index1 = abs(theta_err1)>30;
 err_index2 = abs(theta_err2)>30;
 err_index = abs(theta_err)>30;
-theta_err1(err_index1)
-theta_err1(err_index1)=0;
-theta_err2(err_index2)=0;
-theta_err(err_index)=0;
+theta_err1(err_index1);
+% theta_err1(err_index1)=0;
+% theta_err2(err_index2)=0;
+% theta_err(err_index)=0;
 
 
 plot(theta_elec_in,theta_err,'k','LineWidth',1);
@@ -264,15 +288,22 @@ plot(theta_elec_in,theta_err2,'b','LineWidth',1);
 
 
 hold off;
+box on;
 ylatex = '$Errpr[deg]$';
-ylabel(ylatex,'Interpreter','latex')
+ylabel(ylatex,'Interpreter','latex','FontSize',15)
 xlatex = '${\theta}_r[deg]$';
-xlabel(xlatex,'Interpreter','latex')
+xlabel(xlatex,'Interpreter','latex','FontSize',15)
+
+ylim([-1, 1])
+xlim([0,360])
 
 leg_abc = legend('$\hat{\theta}_r$','${\theta}_1$','${\theta}_2$','Location','northoutside','Numcolumns',3);
 set(leg_abc, 'Interpreter', 'latex')
 set(leg_abc, 'FontSize', 15);
 
+if(save_enable==1)
 filename = sprintf('theta_err_x1y0_sensor_r%d_%s.png',sensor_r,date);
 % saveas(figure_err_theta,filename,'png')
+exportgraphics(ax,filename,'Resolution',600)
+end
 
